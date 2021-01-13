6c1511db4261d4f03bae7596c48e74178051fe05
Adam Murray <admmrry@gmail.com>
2021-01-13 15:34:36 +0000

    First upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mapper-published
+# mapper
+ 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	purge: ['./_site/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+	theme: {
+		colors: {
+			cl__first: 'var(--cl-first)',
+			cl__second: 'var(--cl-second)',
+			cl__third: 'var(--cl-third)',
+			cl__fourth: 'var(--cl-fourth)'
+		}
+	},
+	variants: {},
+	plugins: [],
+}
